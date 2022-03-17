4d84021a40b108f2f690efcc94c958f0f31cd76b
Mirko Đurić <mirkoduric@Mirkos-MacBook-Air.local>
2022-03-17 10:29:41 +0100

    First exercise JS

---
+++ main.js
@@ -0,0 +1,12 @@
+// Boolean = true ili false
+// Koristimo kada pitamo racunar nesto i on nam odgovara sa true ili false
+// == je test da li obje strane imaju isto vrijednost dok === znaci da li je identicno
+
+// console.log(1 == 1);
+// console.log(1 === 2);
+var password = prompt("Pogodite broj");
+if(password===123){
+    console.log("pogodak");
+} else{
+    console.log("Promasaj");
+}
